1d130c1c39ad3c32bfce4e85d38435e4e86aed08
unTouriste <guillaume.tesson@gmail.com>
2022-05-13 09:44:54 +0200

    script utilisation commande bash
    script qui fait appel aux commandes bash de vérification de hash dans un répertoire donné en paramètre et ses sous-répertoires (si l'utilisateur le spécifie)

---
+++ tp2bis/checksum.py
@@ -0,0 +1,31 @@
+import os, subprocess
+
+#fonction de checksum simple qui travaille dans un dossier donné en paramètre
+def checksum(path):
+	method = [ x for x in os.listdir('/bin') if x.endswith("sum") and x != "sum" and not x.startswith("inno")] # récupération des binaires finissant par sum
+	#dictionnaire contenant le chemin vers le binaire associé à une méthode de checksum
+	dict_method = {}
+	for i in method:
+		dict_method[i] = os.path.join("/bin",i)
+	#########################################
+	#création du dictionnaire contenant le nom d'un fichier
+	dict_fic = {}
+	#########################################
+	#test d'un argument passé dans la ligne de commande et que le dossier existe
+	for root, dirs, files in os.walk(path):
+		fic = [ os.path.join(root,x) for x in files]
+	########################################
+	#utilisation du dictionnaire avec les méthodes pour créer la commande de checksum 
+	for i in fic:
+		dict_hash = {}
+		for j in dict_method:
+			cm = [dict_method[j], i]
+			#création du subprocess qui va exécuter la commande et récupérer la valeur retour
+			res = subprocess.run(
+				cm,
+				capture_output=True,
+				text=True)
+			dict_hash[j] = res.stdout.split()[0]
+		dict_fic[i] = dict_hash
+	
+	return dict_fic
---
+++ tp2bis/verifHash.py
@@ -0,0 +1,37 @@
+import argparse, os, time, json #import des bibliothèques systèmes
+
+import checksum, checkRec #import des bibliothèques personnelles
+
+def dir_path(path):
+	if os.path.exists(path):
+		return path
+	else:
+		return "Ce n'est pas un répertoire valide"
+		exit
+
+if __name__ == '__main__':
+	tp1 = time.time()
+	tp2 = 0
+	parser = argparse.ArgumentParser(description="script de calcul de hash utilisant les binaires intégrés à Linux dans le répertoire /bin", conflict_handler='resolve')
+	parser.add_argument("path", help='Dossier où sont contenus les fichiers', type=dir_path)
+	parser.add_argument('-R','--recursive', dest='rec',action="store_true", required=False)
+	parser.add_argument('-j', dest='fic', help="indiquer le nom du fichier sans l'extension .json", required=False)
+	arg = parser.parse_args()
+
+	dictionnaire = {}
+	if arg.rec == False:
+		print("scritp exécuté dans le répertoire renseigné")
+		dictionnaire = checksum.checksum(arg.path)
+	else:
+		print("script exécuté dans le répertoire et les sous-répertoires")
+		dictionnaire = checkRec.checksum_recursif(arg.path)
+
+	if arg.fic:
+		with open(f'{arg.fic}.json', "w") as fp:
+			json.dump(dictionnaire, fp, indent=3)
+		tp2 = time.time()
+	else:
+		with open("fic.json", 'w') as fp:
+			json.dump(dictionnaire,fp, indent=3)
+		tp2 = time.time()
+	print(f"Le script s'est exécuté en {tp2 - tp1:.2F}s.")
